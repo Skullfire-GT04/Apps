c077d0fc518bf8880537700a59d59a342cb75150
Skullfire-GT04 <betterthanthelastonel0l@gmail.com>
2026-06-03 22:31:54 +0530

    Did some cleanup

--- 2D Level Editor (Python Edition)/main.py
+++ 2D Level Editor (Python Edition)/main.py
@@ -5,13 +5,6 @@
 
 import pygame as pg
 from json import load
-from utils.border_frame import BorderFrame
-from utils.console import Console
-from utils.button import Button
-from utils.label import Label
-from utils.input_box import InputBox
-from utils.check_button import CheckButton
-from utils.slider import Slider
 
 class App:
     def __init__(self):
@@ -28,23 +21,6 @@ class App:
         self.clock = pg.time.Clock()
         self.frames = []
         self.independent_widgets = []
-        self.temp = BorderFrame(0.01, 0, 0.3, 1, 10)
-        self.btn = Button(0.1, 0.1, 0.7, 0.14, self.settings["MAIN_FONT"], text_size = 25, padding = 10, bd_radius = 20, text = "Click me !")
-        self.label = Label(0.1, 0.3, 0.7, 0.23, self.settings["MAIN_FONT"], text_size = 20)
-        self.console = Console(0.4, 0.9, 0.2, 0.03, self.settings["MAIN_FONT"])
-        self.btn.set_command(self.change_text)
-        self.independent_widgets.append(Label(0.7, 0.5, 0.3, 0.23, self.settings["MAIN_FONT"], text = "Label2", text_size = 30))
-        self.in_box = InputBox(0.1, 0.7, 0.5, 0.02, self.settings["MAIN_FONT"], text_size = 15)
-        self.slider = Slider(0.5, 0.75, 0.03, 0.2)
-        self.check_button = CheckButton(0.04, 0.9, 0.03, self.settings["MAIN_FONT"])
-        self.slider.set_value(50)
-        self.independent_widgets.append(self.console)
-        self.temp.add_child(self.btn)
-        self.temp.add_child(self.label)
-        self.temp.add_child(self.in_box)
-        self.temp.add_child(self.slider)
-        self.temp.add_child(self.check_button)
-        self.counter = 0
 
     def load_main_settings(self):
         try:
@@ -58,10 +34,6 @@ class App:
     def update(self):
         pass
 
-    def change_text(self):
-        self.counter += 1
-        self.label.set_text(f"Counter: {self.counter}")
-
     # the main loop for the app
     def run(self):
         while self.running:
@@ -72,17 +44,9 @@ class App:
                         widget.calc_new_rect()
                 if (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE) or event.type == pg.QUIT:
                     self.running = False                    
-                if event.type == pg.KEYDOWN and event.key == pg.K_k:
-                    self.label.set_text("Hello there matey! Come aboard !")
-                self.temp.update(event)
+                
 
             self.screen.fill("black")
-
-            self.temp.draw(self.screen)
-            
-            # drawing the independent widgets on top of the frames
-            for i in range(len(self.independent_widgets)):
-                self.independent_widgets[i].draw(self.screen)
 
             pg.display.update()
             self.clock.tick(self.settings["FPS"])
